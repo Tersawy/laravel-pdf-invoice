4d678b5fe93232d2aa324303e680e48d2cce3e90
tersawy <mohamedtersawy96@gmail.com>
2024-11-21 17:01:20 +0200

    Enhance files structure

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,23 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use PDF;
 
 class InvoiceController extends Controller
 {
-    public function index()
-    {
-        return view('invoice');
-    }
-
     public function download()
     {
-        return PDF::loadView('invoice')->download('invoice-' . time() . '-data.pdf');
+        return PDF::loadView('pdf-invoice')->download('invoice-' . time() . '-data.pdf');
     }
 
     public function stream()
     {
-        return PDF::loadView('pdf')->stream('invoice-' . time() . '-data.pdf');
+        return PDF::loadView('pdf-invoice')->stream('invoice-' . time() . '-data.pdf');
     }
 }
